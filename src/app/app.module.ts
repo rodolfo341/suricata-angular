801dc91901d0791d5f16eed9057d0a9a215aa564
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-10-18 18:59:53 -0300

    se crean los cuatro componentes de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SectionComponent } from './components/section/section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    SectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
