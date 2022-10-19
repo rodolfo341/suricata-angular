04916142fed3b8e8637c815b5f8e950c2353343a
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-10-19 18:12:41 -0300

    agregamos interpolacion

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+    nav_link_1: string = "INICIO";
+    nav_link_2: string = "PRODUCTOS";
+    nav_link_3: string = "CONTACTO";
+    
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
